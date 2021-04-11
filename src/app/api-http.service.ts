a8c8aa12c02cdf1ccb05b62d309608a47e1b6f12
WolakMaciej <you@example.com>
2021-04-11 12:15:16 +0200

    api rejestracja

---
+++ src/app/api-http.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {UserCredential} from './domain/UserCredential';
+import {Observable} from 'rxjs';
+
+const BASE_URL = 'http://localhost:8488';
+const HTTP_OPTIONS = {headers: new HttpHeaders({'Content-Type': 'application/json'})};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiHttpService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  userRegister(user: UserCredential): Observable<any>{
+    return this.httpClient.post(`${BASE_URL}/register`, JSON.stringify(user), HTTP_OPTIONS);
+  }
+
+
+}
